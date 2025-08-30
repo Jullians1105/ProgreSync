cb64350e8b645d2a06d230eceea4531f98de9f9f
Felialej <114890853+Felialej@users.noreply.github.com>
2025-08-30 15:17:28 -0500

    Update guard.js

--- Entregas/guard.js
+++ Entregas/guard.js
@@ -1,1 +1,14 @@
+// guard.js
+export function requireRole(rolesPermitidos=[]){
+  try{
+    const sesion = JSON.parse(localStorage.getItem('sesion')||'null');
+    if (!sesion || !rolesPermitidos.includes(sesion.rol)){
+      alert("No tienes permiso para esta vista");
+      location.href = "../../iniciosesion/index.html";
+    }
+    return sesion;
+  }catch{
+    location.href = "../../iniciosesion/index.html";
+  }
+}
 
