c78a6f4a38c6e94a05ba3c4c762e7d2ba169a907
Felialej <114890853+Felialej@users.noreply.github.com>
2025-08-30 14:39:00 -0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,17 @@
-// app.js (placeholder backend)
-console.log("Backend listo para agregar lógica de Express");
+// backend/app.js
+import express from "express";
+import cors from "cors";
 
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Ruta de prueba
+app.get("/", (req, res) => {
+  res.send("Servidor backend funcionando 🚀");
+});
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
