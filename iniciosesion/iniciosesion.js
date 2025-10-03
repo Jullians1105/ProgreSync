3dadb0aa922ece6b53bbb335a1226604319fc34e
Felialej <114890853+Felialej@users.noreply.github.com>
2025-10-02 22:39:12 -0500

    Update iniciosesion.js

--- iniciosesion/iniciosesion.js
+++ iniciosesion/iniciosesion.js
@@ -43,7 +43,7 @@ document.addEventListener("DOMContentLoaded", () => {
       const res = await fetch(`${API_BASE}/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        credentials: "include", // 👈 para que la cookie de sesión viaje
+        credentials: "include", // para que la cookie de sesión viaje
         body: JSON.stringify({ email, password }),
       });
 
@@ -74,5 +74,6 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
 
 
